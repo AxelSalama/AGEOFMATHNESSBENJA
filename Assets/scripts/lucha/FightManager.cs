e7a4ab2341dafac0426cdfcd188728b27e8cc2be
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-11-08 11:34:36 -0300

    duendepicanton

--- Assets/scripts/lucha/FightManager.cs
+++ Assets/scripts/lucha/FightManager.cs
@@ -8,9 +8,11 @@ public class FightManager : MonoBehaviour
     public GameObject Elfo;
     public GameObject Ogro;
     public GameObject Duende;
+    public GameObject Duende2;
     public EnemyData ogro;
     public EnemyData duende;
     public EnemyData elfo;
+    public EnemyData duende2;
     public EnemyLife enemylife;
     void Start()
     {
@@ -33,6 +35,12 @@ public class FightManager : MonoBehaviour
             Instantiate(Ogro, new Vector3(14, 0, 14), Quaternion.identity);
             Ogro.transform.eulerAngles = new Vector3(0, -90, 0);
             Ogro.transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (PlayerPrefs.GetInt("valor") == 4)
+        {
+            Instantiate(Duende2, new Vector3(14, 0, 14), Quaternion.identity);
+            Duende2.transform.eulerAngles = new Vector3(0, -90, 0);
+            Duende2.transform.localScale = new Vector3(1, 1, 1);
         }
     }
 
