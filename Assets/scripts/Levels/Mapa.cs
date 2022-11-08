1f9c448f96cbe06e3bdbc6d426b92dcb89da241d
Ren-Langlois <101406518+Ren-Langlois@users.noreply.github.com>
2022-11-08 11:55:05 -0300

    mapa

---
+++ Assets/scripts/Levels/Mapa.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mapa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReinoSuma()
+    {
+        SceneManager.LoadScene("Reino suma");
+    }
+    public void Reinoresta()
+    {
+        SceneManager.LoadScene("Reino resta");
+    }
+    public void ReinoMulti()
+    {
+        SceneManager.LoadScene("Reino multi");
+    }
+    public void ReinoDivi()
+    {
+        SceneManager.LoadScene("Reino divi");
+    }
+    public void Roma()
+    {
+        SceneManager.LoadScene("roma");
+    }
+}
