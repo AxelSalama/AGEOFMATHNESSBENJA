2b2a99e78bc0cd07029a81504475c5672a54ccb5
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-11-14 14:19:55 -0300

    anda

---
+++ Assets/scripts/lucha/OtroAnimManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtroAnimManager : MonoBehaviour
+{
+    public Animator anim;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim.SetBool("Idle", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
